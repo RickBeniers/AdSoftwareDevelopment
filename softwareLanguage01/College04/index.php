ada1e8d9195b5c6de2137c762102953d9dbdf9a6
RickBeniers <Beniersrick@gmail.com>
2023-09-27 09:46:06 +0200

    added pre made files for college 4. 27-09-2023, 0946.

---
+++ softwareLanguage01/College04/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Server variabelen
+ * SERVER
+ * GET
+ * POST
+ */
+
+
+
+/**
+ * Routing
+ * Waar in de applicatie ben je?
+ *
+ * Voor het gebruik gaan we er gebruik van maken van de volgende opbouw
+ * - index(home)
+ * - contact
+ * - details
+ *      - profiel
+ *      - cijfers
+ *      - ervaringen
+ *      - hobby
+ * - about
+ */
+
+
+
+
+/**
+ * PDO - connect to database
+ *
+ */
